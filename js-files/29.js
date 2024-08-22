9a6ae24abc0aed7e7ef94bd32a9ac570c745676f
Mohsin-Analyst <mohsintheanalyst@gmail.com>
2024-08-23 04:32:30 +0500

    Function Expression

--- js-files/29.js
+++ js-files/29.js
@@ -7,7 +7,6 @@ const myFnc = function(name, age){
 myFnc("Mohsin",22)
 
 
-
 const happyBirthDay = function(){
     console.log("Happy Birthday to you.............")
 }
@@ -15,25 +14,3 @@ const happyBirthDay = function(){
 happyBirthDay()
 
 
-const sumThreeNums = (num1,num2,num3) =>{
-    return num1 + num2 + num3;
-}
-
-const result = sumThreeNums(23,90,54);
-console.log(result)
-
-
-// arrow fnc writing styles
-
-// style 1
-const style1 = name =>  console.log(name);
-style1("ALI")
-
-// style 2
-
-const style2 = (name,age) => console.log(`my name is ${name} & age is ${age}`);
-style2("Mohsin",22)
-
-// style 3
-const style3 = (val1) => val1 + 200;
-console.log(style3(34))
