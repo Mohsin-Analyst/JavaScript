bcdea24a5006c9e612ed718164479ebb4c06ce6f
Mohsin-Analyst <mohsintheanalyst@gmail.com>
2024-08-26 04:49:41 +0500

    reduce() method

---
+++ js-files/43.js
@@ -0,0 +1,32 @@
+// reduce method
+
+const numbers = [120,90,78,66,34];
+
+
+const sum = function (accumulator, currentvalue){
+    return accumulator + currentvalue;
+}
+
+var check = numbers.reduce(sum)
+console.log(check)
+
+const sub = function (accumulator, currentvalue){
+    return accumulator - currentvalue;
+}
+
+var check = numbers.reduce(sub)
+console.log(check)
+
+const mul = function (accumulator, currentvalue){
+    return accumulator * currentvalue;
+}
+
+var check = numbers.reduce(mul)
+console.log(check)
+
+const div = function (accumulator, currentvalue){
+    return accumulator / currentvalue;
+}
+
+var check = numbers.reduce(div)
+console.log(check)
